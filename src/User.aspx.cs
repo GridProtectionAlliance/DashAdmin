917a6518fb3dbfdde01fdd36dc5b99978832543a
jeffhwalker <jwalker@gridprotectionalliance.org>
2016-02-12 09:21:46 -0500

    Initial

---
+++ src/User.aspx.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class User : System.Web.UI.Page
+{
+    // Right Panel userGroupDataSource
+    private String DeleteCommandUserGroup = "DELETE FROM [UserGroup] WHERE [userID] = {0}";
+    private String InsertCommandUserGroup = "INSERT INTO [UserGroup] ([groupID], [userID]) VALUES ({0},{1})";
+    private String SelectCommandUserGroup = "SELECT [Group].[ID], [groupName] FROM [UserGroup] left join [Group] on [Group].[ID] = [UserGroup].[groupID] and [UserGroup].[userID] = {0} where [Active] = 1";
+    private String UpdateCommandUserGroup = "";
+
+    // Left Panel groupDataSource
+    //private String DeleteCommandGroup = "DELETE FROM [Group] WHERE [ID] = @ID";
+    //private String InsertCommandGroup = "INSERT INTO [Group] ([groupName], [Active]) VALUES (@groupName, @Active)";
+    private String SelectCommandGroup = "SELECT [Group].[ID], [groupName] FROM [Group] left outer join [UserGroup] on [Group].[ID] = [UserGroup].[groupID] and [UserGroup].[userID] = {0} where [UserGroup].[groupID] is NULL and [Active] = 1";
+    //private String UpdateCommandGroup = "UPDATE [Group] SET [groupName] = @groupName, [Active] = @Active WHERE [ID] = @ID";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void MoveItems(bool isAdd)
+    {
+        if (isAdd)// means if you add items to the right box
+        {
+            for (int i = ListBox1.Items.Count - 1; i >= 0; i--)
+            {
+                if (ListBox1.Items[i].Selected)
+                {
+                    ListBox2.Items.Add(ListBox1.Items[i]);
+                    ListBox2.ClearSelection();
+                    ListBox1.Items.Remove(ListBox1.Items[i]);
+                }
+            }
+        }
+        else // means if you remove items from the right box and add it back to the left box
+        {
+            for (int i = ListBox2.Items.Count - 1; i >= 0; i--)
+            {
+                if (ListBox2.Items[i].Selected)
+                {
+                    ListBox1.Items.Add(ListBox2.Items[i]);
+                    ListBox1.ClearSelection();
+                    ListBox2.Items.Remove(ListBox2.Items[i]);
+                }
+            }
+        }
+    }
+
+    private void MoveAllItems(bool isAddAll)
+    {
+        if (isAddAll)// means if you add ALL items to the right box
+        {
+            for (int i = ListBox1.Items.Count - 1; i >= 0; i--)
+            {
+                ListBox2.Items.Add(ListBox1.Items[i]);
+                ListBox2.ClearSelection();
+                ListBox1.Items.Remove(ListBox1.Items[i]);
+            }
+        }
+        else // means if you remove ALL items from the right box and add it back to the left box
+        {
+            for (int i = ListBox2.Items.Count - 1; i >= 0; i--)
+            {
+                ListBox1.Items.Add(ListBox2.Items[i]);
+                ListBox1.ClearSelection();
+                ListBox2.Items.Remove(ListBox2.Items[i]);
+            }
+        }
+    }
+
+    protected void ButtonAdd_Click(object sender, EventArgs e)
+    {
+        MoveItems(true);// true since we add
+    }
+
+    protected void ButtonRemove_Click(object sender, EventArgs e)
+    {
+        MoveItems(false); // false since we remove
+    }
+
+    protected void ButtonAddAll_Click(object sender, EventArgs e)
+    {
+        MoveAllItems(true); // true since we add all
+    }
+
+    protected void ButtonRemoveAll_Click(object sender, EventArgs e)
+    {
+        MoveAllItems(false); // false means re remove all
+    }
+
+    protected void ButtonEdit_Click(object sender, EventArgs e)
+    {
+        String stuff = "";
+        Panel2.Visible = true;
+
+        var btn = (Button)sender;
+        var item = (ListViewItem)btn.NamingContainer;
+        Label thelabel = (Label)item.FindControl("IDLabel");
+        stuff = thelabel.Text;
+
+        ListBox1.Items.Clear();
+        ListBox2.Items.Clear();
+
+        groupDataSource.SelectCommand = String.Format(SelectCommandGroup, stuff);
+        userGroupDataSource.SelectCommand = String.Format(SelectCommandUserGroup, stuff);
+    }
+
+    protected void ButtonSave_Click(object sender, EventArgs e)
+    {
+        String stuff = "";
+        Panel2.Visible = false;
+
+        var btn = (Button)sender;
+        var item = (ListViewItem)btn.NamingContainer;
+        Label thelabel = (Label)item.FindControl("IDLabel");
+        stuff = thelabel.Text;
+    }
+
+    protected void ButtonCancel_Click(object sender, EventArgs e)
+    {
+        Panel2.Visible = false;
+    }
+
+    protected void ButtonDelete_Click(object sender, EventArgs e)
+    {
+        String userId = "";
+        Panel2.Visible = false;
+
+        var btn = (Button)sender;
+        var item = (ListViewItem)btn.NamingContainer;
+        Label thelabel = (Label)item.FindControl("IDLabel");
+        userId = thelabel.Text;
+
+        userGroupDataSource.DeleteCommand = String.Format(DeleteCommandUserGroup, userId);
+        userGroupDataSource.Delete();
+
+    }
+
+    protected void ButtonInsert_Click(object sender, EventArgs e)
+    {
+
+        Panel2.Visible = false;
+    }
+
+    protected void ButtonUpdate_Click(object sender, EventArgs e)
+    {
+        String userId = "";
+        Panel2.Visible = false;
+
+        var btn = (Button)sender;
+        var item = (ListViewItem)btn.NamingContainer;
+        Label thelabel = (Label)item.FindControl("IDLabel1");
+        userId = thelabel.Text;
+
+        // Clear all for this user first.
+
+        userGroupDataSource.DeleteCommand = String.Format(DeleteCommandUserGroup, userId);
+        userGroupDataSource.Delete();
+
+        // iterate through Listbox2 entries adding each for this user.
+
+        for (int i = 0; i < ListBox2.Items.Count; i++)
+        {
+            userGroupDataSource.InsertCommand = String.Format(InsertCommandUserGroup, ListBox2.Items[i].Value, userId);
+            userGroupDataSource.Insert();
+        }
+    }
+}
